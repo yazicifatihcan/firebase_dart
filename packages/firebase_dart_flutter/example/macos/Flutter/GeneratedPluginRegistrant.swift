fddc67c782d736766bb6c2b60352cf6880691cb3
Fatih Can Yazıcı <72442904+yazicifatihcan@users.noreply.github.com>
2024-07-19 21:52:19 +0300

    fix: intl version changed.

--- packages/firebase_dart_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/firebase_dart_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,15 @@
 import FlutterMacOS
 import Foundation
 
+import google_sign_in_ios
 import package_info
-import path_provider_macos
+import path_provider_foundation
 import sign_in_with_apple
 import uni_links_desktop
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
